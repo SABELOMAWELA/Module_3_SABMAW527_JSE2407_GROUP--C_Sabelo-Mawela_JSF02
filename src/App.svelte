ff72cd2fcc4435b52e84b78d271449c5902902dc
SABELOMAWELA <Mawelas63@gmail.com>
2024-07-24 16:36:43 +0200

    "Added ProductlistLoadingState component to App.svelte"

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,10 @@
 <script>
   import Navbar from './Components/navbar.svelte';
   import Searchsort from './Components/Search&sort.svelte';
+  import ProductlistLoadingState from './Components/Productlist&LoadingState.svelte';
 </script>
 
 <Navbar title="SwiftCart" />
 <Searchsort />
+<ProductlistLoadingState />
 
